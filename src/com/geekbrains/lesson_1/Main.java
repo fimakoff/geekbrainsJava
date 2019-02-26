b3b741de39ee0a5ae446f692848116812d5f2870
fimakoff <fimakoff@gmail.com>
2019-02-26 10:27:33 +0300

    add func true/false

--- src/com/geekbrains/lesson_1/Main.java
+++ src/com/geekbrains/lesson_1/Main.java
@@ -14,12 +14,24 @@ public class Main {
 
         char ch = 'a';
         String str = "Hello, everyone!";
-        
+
         System.out.println(num(1,2,3,4));
+        System.out.println(isTrue(10,11));
     }
 
     static double num (int a, int b, int c, int d){
         double res = a * (b + ((double)c / d));
         return res;
+    }
+
+    public static boolean isTrue (int a, int b){
+        boolean sumIsTrue;
+        int sum = a + b;
+        if(sum > 10 && sum <= 20){
+            sumIsTrue = true;
+        }else{
+            sumIsTrue = false;
+        }
+        return sumIsTrue;
     }
 }
