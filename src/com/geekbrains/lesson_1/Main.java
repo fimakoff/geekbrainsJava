088a5af3186c6a86fc95bd4c0e7d49d4a46dd3c8
fimakoff <fimakoff@gmail.com>
2019-02-26 10:15:30 +0300

    add nums

---
+++ src/com/geekbrains/lesson_1/Main.java
@@ -0,0 +1,18 @@
+package com.geekbrains.lesson_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte b = 17;
+        short sh = 543;
+        int in = 543;
+        long lo = 123456789L;
+
+        float fl = 123.45f;
+        double dou = 123.45;
+
+        char ch = 'a';
+        String str = "Hello, everyone!";
+    }
+}
